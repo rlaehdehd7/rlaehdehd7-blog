173d5a0fcb88fb4291dffcb24745b8f91918181e
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-05-28 23:17:57 +0900

    feat: 서버 컴포넌트와 클라이언트 컴포넌트 학습

--- lib/notion.ts
+++ lib/notion.ts
@@ -88,7 +88,7 @@ export const getPostBySlug = async (
   };
 };
 
-export const getPublishedPosts = async (tag?: string): Promise<Post[]> => {
+export const getPublishedPosts = async (tag?: string, sort?: string): Promise<Post[]> => {
   const response = await notion.databases.query({
     database_id: process.env.NOTION_DATABASE_ID!,
     filter: {
@@ -114,7 +114,7 @@ export const getPublishedPosts = async (tag?: string): Promise<Post[]> => {
     sorts: [
       {
         property: 'Date',
-        direction: 'descending',
+        direction: sort === 'latest' ? 'descending' : 'ascending',
       },
     ],
   });
