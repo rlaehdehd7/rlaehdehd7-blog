7587e24656f597d9c22afceedb92f6bce822882e
rlaehdehd7 <rlaehdehd7@gmail.com>
2025-05-29 20:25:40 +0900

    feat: 로딩 UI 및 스트리밍

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import TagSection from '@/app/_components/TagSection';
 import ProfileSection from '@/app/_components/ProfileSection';
 import ContactSection from '@/app/_components/ContactSection';
-import { getTags } from '@/lib/notion';
 import HeaderSection from './_components/HeaderSection';
-import PostListClient from '@/components/features/blog/PostList.client';
+import PostListSuspense from '@/components/features/blog/PostListSuspense';
+import { getTags } from '@/lib/notion';
+import { Suspense } from 'react';
+import TagSectionClient from '@/app/_components/TagSection.client';
+import TagSectionSkeleton from '@/app/_components/TagSectionSkeleton';
+import PostListSkeleton from '@/components/features/blog/PostListSkeleton';
 
 interface HomeProps {
   searchParams: Promise<{ tag?: string; sort?: string }>;
@@ -12,21 +15,26 @@ interface HomeProps {
 export default async function Home({ searchParams }: HomeProps) {
   const { tag, sort } = await searchParams;
   const selectedTag = tag || '전체';
-  // const selectedSort = sort || 'latest';
-  const [tags] = await Promise.all([getTags()]);
+  const selectedSort = sort || 'latest';
+
+  const tags = getTags();
 
   return (
     <div className="container py-8">
       <div className="grid grid-cols-[200px_1fr_220px] gap-6">
         {/* 좌측 사이드바 */}
         <aside>
-          <TagSection tags={tags} selectedTag={selectedTag} />
+          <Suspense fallback={<TagSectionSkeleton />}>
+            <TagSectionClient tags={tags} selectedTag={selectedTag} />
+          </Suspense>
         </aside>
         <div className="space-y-8">
           {/* 섹션 제목 */}
           <HeaderSection selectedTag={selectedTag} />
           {/* 블로그 카드 그리드 */}
-          <PostListClient />
+          <Suspense fallback={<PostListSkeleton />}>
+            <PostListSuspense selectedTag={selectedTag} selectedSort={selectedSort} />
+          </Suspense>
         </div>
         {/* 우측 사이드바 */}
         <aside className="flex flex-col gap-6">
